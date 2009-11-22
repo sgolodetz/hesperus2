cc3cc6a02e63dce99cca7cec4d6d6f2b2dea191b
sgolodetz <sgolodetz@gxstudios.net>
2009-11-22 15:42:15 +0000

    Wrote a (very) simple test program for AngelScript 2.17.2.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus2@5 28fabe82-442e-48f8-b112-272d2d8048be

--- trunk/source/tests/test-angelscript-2.17.2/main.cpp
+++ trunk/source/tests/test-angelscript-2.17.2/main.cpp
@@ -3,7 +3,11 @@
  * Copyright Stuart Golodetz, 2009. All rights reserved.
  ***/
 
+#include <angelscript.h>
+
 int main()
 {
+	asIScriptEngine *engine = asCreateScriptEngine(ANGELSCRIPT_VERSION);
+	engine->Release();
 	return 0;
 }
