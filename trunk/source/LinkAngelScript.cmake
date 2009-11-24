4e889a4a12bfbcdceef34970764b46eec8e99cdf
sgolodetz <sgolodetz@gxstudios.net>
2009-11-24 20:15:31 +0000

    Updated LinkAngelScript.cmake to use the correct AngelScript library when building in debug configuration on Linux.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus2@27 28fabe82-442e-48f8-b112-272d2d8048be

--- trunk/source/LinkAngelScript.cmake
+++ trunk/source/LinkAngelScript.cmake
@@ -2,4 +2,8 @@
 # LinkAngelScript.cmake #
 #########################
 
-TARGET_LINK_LIBRARIES(${targetname} debug angelscriptd optimized angelscript)
+IF(MSVC_IDE)
+	TARGET_LINK_LIBRARIES(${targetname} debug angelscriptd optimized angelscript)
+ELSE(MSVC_IDE)
+	TARGET_LINK_LIBRARIES(${targetname} angelscript)
+ENDIF(MSVC_IDE)
