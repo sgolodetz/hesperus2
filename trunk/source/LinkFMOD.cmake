e112218dc813e2939f073d9d16d7c51eff7d9ddd
sgolodetz <sgolodetz@gxstudios.net>
2009-11-24 21:06:07 +0000

    Updated LinkFMOD.cmake to work on Linux as well.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus2@29 28fabe82-442e-48f8-b112-272d2d8048be

--- trunk/source/LinkFMOD.cmake
+++ trunk/source/LinkFMOD.cmake
@@ -4,4 +4,6 @@
 
 IF(MSVC_IDE)
 	TARGET_LINK_LIBRARIES(${targetname} debug fmodexL_vc optimized fmodex_vc)
+ELSE(MSVC_IDE)
+	TARGET_LINK_LIBRARIES(${targetname} debug fmodexL optimized fmodex)
 ENDIF(MSVC_IDE)
