7af2c7699a0005be9622663c2cab5c31600e3203
sgolodetz <sgolodetz@gxstudios.net>
2009-11-24 18:51:14 +0000

    Added a setup zip and build instructions for lodepng-20080927.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus2@24 28fabe82-442e-48f8-b112-272d2d8048be

--- trunk/source/SetTarget.cmake
+++ trunk/source/SetTarget.cmake
@@ -4,8 +4,4 @@
 
 SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${hesperus2_BINARY_DIR}/bin/tests/${targetname}/bin)
 ADD_EXECUTABLE(${targetname} ${sources})
-IF(MSVC_IDE)
-	# A hack to get around the "Debug" and "Release" directories Visual Studio tries to add
-	SET_TARGET_PROPERTIES(${targetname} PROPERTIES PREFIX "../")
-	SET_TARGET_PROPERTIES(${targetname} PROPERTIES IMPORT_PREFIX "../")
-ENDIF(MSVC_IDE)
+INCLUDE(${hesperus2_SOURCE_DIR}/VCTargetHack.cmake)
