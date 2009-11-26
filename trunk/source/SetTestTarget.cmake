6171930573d5519d9e50b4ea97d18ea6575e78fa
sgolodetz <sgolodetz@gxstudios.net>
2009-11-26 19:01:00 +0000

    Changed the names of the debug executables for test and tool programs.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus2@50 28fabe82-442e-48f8-b112-272d2d8048be

--- trunk/source/SetTestTarget.cmake
+++ trunk/source/SetTestTarget.cmake
@@ -5,3 +5,5 @@
 SET(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${hesperus2_BINARY_DIR}/bin/tests/${targetname}/bin)
 ADD_EXECUTABLE(${targetname} ${sources})
 INCLUDE(${hesperus2_SOURCE_DIR}/VCTargetHack.cmake)
+
+SET_TARGET_PROPERTIES(${targetname} PROPERTIES DEBUG_OUTPUT_NAME "${targetname}_d")
