db6ef3a1720b252c9d08095fd316e1eec4fe0fc8
sgolodetz <sgolodetz@gxstudios.net>
2009-12-01 21:53:42 +0000

    Implemented determine_executable_location() for Linux systems.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus2@68 28fabe82-442e-48f8-b112-272d2d8048be

--- trunk/source/engine/core/hesp/io/util/DirectoryFinder.cpp
+++ trunk/source/engine/core/hesp/io/util/DirectoryFinder.cpp
@@ -4,11 +4,17 @@
  ***/
 
 #include "DirectoryFinder.h"
+
+#include <vector>
+
+#include <hesp/exceptions/Exception.h>
+
 namespace bf = boost::filesystem;
 
 #if defined(_WIN32)
 	#include <windows.h>
 #elif defined(__linux__)
+	#include <sstream>
 	#include <unistd.h>
 #endif
 
@@ -16,16 +22,31 @@ namespace hesp {
 
 bf::path determine_executable_location()
 {
+	const int BUFFER_SIZE = 512;
+	std::vector<char> buffer(BUFFER_SIZE+1);
+
 #if defined(_WIN32)
-	std::string s;
-	s.resize(512);
-	::GetModuleFileName(NULL, &s[0], 512);
-	return s;
+	::GetModuleFileName(NULL, &buffer[0], BUFFER_SIZE);
 #elif defined(__linux__)
-	#error NYI
+	// Get the process ID.
+	int pid = getpid();
+
+	// Construct a path to the symbolic link pointing to the process executable.
+	// This is at /proc/<pid>/exe on Linux systems (we hope).
+	std::ostringstream oss;
+	oss << "/proc/" << pid << "/exe";
+	std::string link = oss.str();
+
+	// Read the contents of the link.
+	int count = readlink(link.c_str(), &buffer[0], BUFFER_SIZE);
+	if(count == -1) throw Exception("Could not read symbolic link");
+	buffer[count] = '\0';
 #else
-	#error Can't yet determine the executable location on this platform
+	#error Cannot yet determine the executable location on this platform
 #endif
+
+	std::string s = &buffer[0];
+	return s;
 }
 
 }
