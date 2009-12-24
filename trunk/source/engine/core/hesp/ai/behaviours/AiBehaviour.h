ec657c67f9a6779931ab916dabddf7df8d41d96c
sgolodetz <sgolodetz@gxstudios.net>
2009-12-24 15:29:42 +0000

    Started the class-level design for the AI.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus2@114 28fabe82-442e-48f8-b112-272d2d8048be

---
+++ trunk/source/engine/core/hesp/ai/behaviours/AiBehaviour.h
@@ -0,0 +1,20 @@
+/***
+ * hesperus: AiBehaviour.h
+ * Copyright Stuart Golodetz, 2009. All rights reserved.
+ ***/
+
+#ifndef H_HESP_AIBEHAVIOUR
+#define H_HESP_AIBEHAVIOUR
+
+namespace hesp {
+
+class AiBehaviour
+{
+	//#################### DESTRUCTOR ####################
+public:
+	virtual ~AiBehaviour();
+};
+
+}
+
+#endif
