85081f4f791354307aee801c75994409dde6eca4
sgolodetz <sgolodetz@gxstudios.net>
2010-01-12 18:08:09 +0000

    Changed LinkFMOD.cmake to cope with 64-bit Linux.
    git-svn-id: svn://edison.comlab.ox.ac.uk/res07/stug/hesperus2@141 28fabe82-442e-48f8-b112-272d2d8048be

--- trunk/source/LinkFMOD.cmake
+++ trunk/source/LinkFMOD.cmake
@@ -2,8 +2,18 @@
 # LinkFMOD.cmake #
 ##################
 
-IF(MSVC_IDE)
-	TARGET_LINK_LIBRARIES(${targetname} debug fmodexL_vc optimized fmodex_vc)
-ELSE(MSVC_IDE)
-	TARGET_LINK_LIBRARIES(${targetname} debug fmodexL optimized fmodex)
-ENDIF(MSVC_IDE)
+IF(CMAKE_SIZEOF_VOID_P EQUAL 4)
+	# 32-bit
+	IF(MSVC_IDE)
+		TARGET_LINK_LIBRARIES(${targetname} debug fmodexL_vc optimized fmodex_vc)
+	ELSE(MSVC_IDE)
+		TARGET_LINK_LIBRARIES(${targetname} debug fmodexL optimized fmodex)
+	ENDIF(MSVC_IDE)
+ELSE(CMAKE_SIZEOF_VOID_P EQUAL 4)
+	# 64-bit
+	IF(MSVC_IDE)
+		# TODO
+	ELSE(MSVC_IDE)
+		TARGET_LINK_LIBRARIES(${targetname} debug fmodex64L optimized fmodex64)
+	ENDIF(MSVC_IDE)
+ENDIF(CMAKE_SIZEOF_VOID_P EQUAL 4)
