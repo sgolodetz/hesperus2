cada4785cdc1eb854997060a042a1f0842b4cc51
sgolodetz <sgolodetz@gxstudios.net>
2011-10-28 01:42:55 +0100

    Continued getting hesperus2 working again.

--- source/VCTargetHack.cmake
+++
@@ -1,9 +0,0 @@
-######################
-# VCTargetHack.cmake #
-######################
-
-IF(MSVC_IDE)
-	# A hack to get around the "Debug" and "Release" directories Visual Studio tries to add
-	SET_TARGET_PROPERTIES(${targetname} PROPERTIES PREFIX "../")
-	SET_TARGET_PROPERTIES(${targetname} PROPERTIES IMPORT_PREFIX "../")
-ENDIF(MSVC_IDE)
